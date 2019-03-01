f72d3e2597317d0c363ff60ae96539d7833a6d60
Cameron Yick <cameron.yick@enigma.com>
2019-03-01 01:31:33 -0500

    Get margin chart working on both sides

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -6,17 +6,18 @@ import withData from "./withData";
 import { scaleSqrt, scaleLinear } from "d3-scale";
 import {  max, histogram } from "d3-array";
 
-const CHART_WIDTH = 1000;
+const CHART_WIDTH = 850;
 const CHART_HEIGHT = 700;
 const CHART_MARGIN = { left: 60, bottom: 60, top: 50, right: 30 };
 const CHART_DIMS = [CHART_WIDTH, CHART_HEIGHT];
 
 const X_MARGIN_CHART_HEIGHT = 200;
 const X_MARGIN_CHART_DIMS = [CHART_WIDTH, X_MARGIN_CHART_HEIGHT];
-const X_NUM_TICKS = 40;
+const X_NUM_TICKS = 50;
 
-const Y_MARGIN_CHART_WIDTH = 300;
+const Y_MARGIN_CHART_WIDTH = 200;
 const Y_MARGIN_CHART_DIMS = [Y_MARGIN_CHART_WIDTH, CHART_HEIGHT];
+const Y_NUM_TICKS = 50;
 
 const Scatterplot = props => {
   const radiusScale = scaleSqrt()
@@ -50,14 +51,11 @@ const MarginPlotX = props => {
                           .domain(xScale.domain())
                           .thresholds(xScale.ticks(X_NUM_TICKS))
                           .value(d => d.width);
-
   const bins = makeHistogram(data);
-
-  console.log(bins);
 
   return <OrdinalFrame
     size={X_MARGIN_CHART_DIMS}
-    margin={{ left: 60, bottom: 10, top: 50, right: 30 }}
+    margin={{ left: 60, bottom: 0, top: 50, right: 30 }}
     data={bins}
     projection={'vertical'}
     type={'bar'}
@@ -68,22 +66,34 @@ const MarginPlotX = props => {
 
 //
 const MarginPlotY = props => {
-  return <XYFrame
+  const { data } = props;
+  const yScale = scaleLinear()
+    .domain([-max(props.data, d => d.height), 0]) // upside down
+    .range([CHART_MARGIN.top, CHART_HEIGHT - CHART_MARGIN.bottom]);
+
+  const makeHistogram = histogram()
+    .domain(yScale.domain())
+    .thresholds(yScale.ticks(Y_NUM_TICKS))
+    .value(d => -d.height);
+  const bins = makeHistogram(data);
+
+  return <OrdinalFrame
     size={Y_MARGIN_CHART_DIMS}
-    margin={{ left: 60, bottom: 60, top: 50, right: 30 }}
-    points={props.data}
-    xAccessor="width"
-    yAccessor="height"
-  />;;
+    margin={{ left: 0, bottom: CHART_MARGIN.bottom, top: CHART_MARGIN.top, right: 30 }}
+    data={bins}
+    projection={'horizontal'}
+    type={'bar'}
+    oAccessor={d => d.x0} // or x1
+    rAccessor={d => d.length}
+  />;
 };
 
 const Chart = props => {
   return (
     <div>
       <MarginPlotX data={props.data} />
-      <Scatterplot data={props.data} />
-
-      <MarginPlotY data={props.data} />
+      <div style={{ display: 'inline-block' }}><Scatterplot data={props.data} /></div>
+      <div style={{ display: 'inline-block' }}> <MarginPlotY data={props.data} style={{ display: 'inline-block' }} /></div>
     </div>
   );
 };
